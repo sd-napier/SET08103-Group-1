9cdac99c823db60c12ce070d1898e9129e381506
Stuart C. Alexander <stualexander@live.co.uk>
2025-09-22 21:36:48 +0100

    creation of database in docker container
    created a virtual LAMP stack with phpmyadmin, added the database creation queries to the database 'SET08103'

---
+++ docker/database/index.php
@@ -0,0 +1,5 @@
+<?php 
+echo "Hello and welcome to Wednesday Group 1's web server!";
+    phpinfo(); # this will be the web page that is served as the default on our apache server
+                # phpinfo(); <-- prints out the current server config.
+?> 
