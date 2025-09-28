1327b6dd334e34dbf5b8458db2dab8af5cd19998
DGBCALI03 <dominicbenell@icloud.com>
2025-09-28 15:45:41 +0100

    Added new dependencies for mysql databse connection.

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -1,17 +1,47 @@
 package com.napier.sem;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Import java classes used to talk to the database.
+import java.sql.*;
+
+/**
+ * Entry point for the application.
+ * Connects to the MySQL datase and prints population info.
+ * @author Socrates Davidopoulos
+ * @author dominicbenell
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Connection details
+        String url = "jdbc:mysql://localhost:3306/SET08103?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+        String username = "WedTeam1";
+        String password = "reallySecurePassword!";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // placeholder for parameter, set later with stmt.setString
+        String sql = "SELECT Population FROM country WHERE Name = ?";
+
+        // Opens connection and prepares SQL query.
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            // Set name of country to United Kingdom.
+            stmt.setString(1, "United Kingdom");
+            ResultSet rs = stmt.executeQuery();     // Executes query and stores in rs.
+
+            // If result at least one row.
+            if (rs.next()) {
+                long population = rs.getLong("population");
+                System.out.println("The population of the UK is " + population);
+            } else {
+                // Query returned zero rows.
+                System.out.println("No population data found for the United Kingdom\".");
+            }
+        // Connection to database failed.
+        } catch(SQLException e){
+            System.out.println("Database connection failed: "+ e.getMessage());
+
+        } catch (Exception e) {
+            // If error is something like wrong column name or null pointer.
+            System.out.println("Unexpected error: " + e.getMessage());
         }
     }
 }
