f243439cdf46702fee3cdc5bbce5abe3a4cb1dc9
40690723 <40690723@live.napier.ac.uk>
2025-10-15 08:59:31 +0100

    Added comments to files

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
+# Use latest version of openJDK
 FROM openjdk:latest
+#Copy jar to working directory
 COPY ./target/App.jar /tmp
+#Set working directory
 WORKDIR /tmp
+#entrypoint
 ENTRYPOINT ["java", "-jar", "App.jar"]
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,6 +2,11 @@ package com.napier.sem;
 
 import java.sql.*;
 
+/**
+ * App class used for loading up database drivers -
+ * and establishing connection to the database in preparation for interaction
+ * App will attempt to connect 100 times to the database before failing connection
+ */
 public class App
 {
     public static void main(String[] args)
