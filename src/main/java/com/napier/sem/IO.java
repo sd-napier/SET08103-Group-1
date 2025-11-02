cf00d6ce436bf77b5338f4d8201e844f1956f857
Stuart C. Alexander <stualexander@live.co.uk>
2025-11-02 12:32:38 +0000

    Created IO class for reading user input and parsing integers

---
+++ src/main/java/com/napier/sem/IO.java
@@ -0,0 +1,33 @@
+package com.napier.sem;
+
+import java.util.Scanner;
+
+/**
+ * @author stual
+ * @date 02/11/2025
+ */
+public class IO {
+
+    // Instance of the scanner
+    private final Scanner input = new Scanner(System.in);
+
+    // Returns instance of the scanner for use in other classes
+    public Scanner getInput() {
+        return input;
+    }
+
+    //This method takes input and parses it to an integer, if no number was entered then returns a '0'
+    public int getInteger() {
+
+        String userInput = getInput().nextLine();
+        int result;
+
+        try {
+            result = Integer.parseInt(userInput);
+        }
+        catch (NumberFormatException e){
+            result = 0 ;
+        }
+        return result;
+    }
+}
