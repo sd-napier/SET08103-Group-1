dadc4827c2f72434b930fa73b984da199f8188ac
Stuart C. Alexander <stualexander@live.co.uk>
2025-11-04 13:32:19 +0000

    First two reports added and formatted to .md output file.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -7,8 +7,8 @@ public class App
     public static void main(String[] args) {
 
         Controller cont = new Controller();
-        cont.dockerTestConnection();
-        //cont.LocalTestConnection();
+        //cont.dockerTestConnection();
+        cont.LocalTestConnection();
     }
 
 }
